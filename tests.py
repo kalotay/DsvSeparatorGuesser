5d1e6dbacaf86f902940ecf92e84d3e7ca972edb
Daniel Kalotay <d.kalotay@gmail.com>
2014-08-25 18:22:57 +0100

    refactor tests

--- tests.py
+++ tests.py
@@ -3,16 +3,15 @@ from separatorguesser import guess
 from unittest import TestCase
 
 class DsvSeparatorGuesserTests(TestCase):
+    def assertSeparators(self, filename, field, record):
+        with open(filename) as f:
+            g = guess(f)
+            self.assertEqual(g.field_separator, field)
+            self.assertEqual(g.record_separator, record)
 
     def test_csv(self):
-        with open('test.csv') as f:
-            g = guess(f)
-            self.assertEqual(g.field_separator, b',')
-            self.assertEqual(g.record_separator, b'\n')
+        self.assertSeparators('test.csv', b',', b'\n')
    
     def test_tsv(self):
-        with open('test.tsv') as f:
-            g = guess(f)
-            self.assertEqual(g.field_separator, b'\t')
-            self.assertEqual(g.record_separator, b'\n')
+        self.assertSeparators('test.tsv', b'\t', b'\n')
    
